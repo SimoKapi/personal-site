73967e7adbb46c38249b2f3030290eac70d95827
Simon Kapicka <simon.kapicka@gmail.com>
2026-02-01 20:52:59 +0100

    update rasterizer description

--- src/pages/projects/Rasterizer.tsx
+++ src/pages/projects/Rasterizer.tsx
@@ -4,6 +4,7 @@ import { BlockMath, InlineMath } from 'react-katex';
 import Fail from '/imgs/projects/rasterizer/fail.png';
 import Projection from '/imgs/projects/rasterizer/projection.png';
 import Shading from '/imgs/projects/rasterizer/shading.png';
+import CopyableHeader from "../../CopyableHeader";
 
 function Rasterizer() {
     return (
@@ -25,7 +26,7 @@ function Rasterizer() {
           </div>
 
           <h1 className='subheader-project'>So... how does it work?</h1>
-          <h2 className='subsubheader-project'>Spatial transformation</h2>
+          <CopyableHeader id="spatial-transform" className="subsubheader-project">Spatial transformation</CopyableHeader>
           <p>The camera <InlineMath math='C = (C_x, C_y, C_z)'/> can have a non-zero position and rotation, as is the case in almost any 3D graphics engine with a movable scene. This is why all vertices must be transformed so that they are relative to the camera, in other words, placing the camera at the origin <InlineMath math='O = (0, 0, 0)'/>. To do this, we simply subtract the position of the camera from each vertex <InlineMath math='P = (P_x, P_y, P_z)'/>, resulting in <InlineMath math="P' = (P_x - C_x, P_y - C_y, P_z - C_z)"/>.</p>
           <p>Now the camera is at the origin. Next, we must apply rotational transformations, to rotate the scene around the camera. This is done by rotating by multiplying the positions of each vertex by a rotation matrix, rotating them around the origin. Each axis of rotation has a different matrix, as shown below.</p>
           <BlockMath math={
@@ -52,7 +53,7 @@ function Rasterizer() {
             \\end{pmatrix}`
             }/>
 
-          <h2 className="subsubheader-project">Perpsective transformation</h2>
+          <CopyableHeader id="perspective-transform" className="subsubheader-project">Perpsective transformation</CopyableHeader>
           <p>Now we have our transformed vertices. Next, we must project them from three dimensions onto two, using perspective transformation calculations. They're actually really simple to derive, as it all comes from the similarity of triangles.</p>
           <p>The projection of point <InlineMath math='P = (P_x, P_y, P_z)'/> onto a screen positioned at <InlineMath math='E = (E_x, E_y, E_z)'/> relative to the camera can be visualized on a right triangle below, viewed from the side. This gives the projected <InlineMath math='y'/> coordinate, and the same visualization can be used to obtain the projected <InlineMath math='x'/> coordinate.</p>
           <p>The two similar triangles created both start in the origin <InlineMath math='O'/>, and stretch out to the <InlineMath math='y'/> point or the <InlineMath math='P_y'/> edge, respectively. Because they are similar, the ratios of their side lengths must remain the same; thus, <InlineMath math='\frac{P_z}{P_y} = \frac{E_z}{Ey}'/></p>
@@ -60,7 +61,7 @@ function Rasterizer() {
           <img src={Projection} className="project-img m-auto invert-on-dark pb-2"/>
           <p>The same approach can be taken on a different axis to find <InlineMath math='|Ex| = E_z \cdot \frac{P_z}{P_x}'/></p>
 
-          <h2 className="subsubheader-project">Backface culling</h2>
+          <CopyableHeader id="backface-culling" className="subsubheader-project">Backface culling</CopyableHeader>
           <p>These are all the steps necessary to transform points from a 3D environment into a 2D environment. However, there are optimizations that can be done — including backface culling, where only the faces that are facing the camera are rendered.</p>
           <p>In solid objects, this makes sense, as any faces facing away will be obscured by other faces oriented towards the camera. Since our vertices are already transformed relative to the camera, this is simple to check.</p>
           <p>We first obtain the normal vector of a given face by utilizing the cross product of vectors. Let's represent a face with three points, <InlineMath math='A\ B\ C'/>. Two vectors are <InlineMath math='\vec{AB}'/> and <InlineMath math='\vec{AC}'/>. The cross product of vectors is shown below</p>
@@ -74,11 +75,14 @@ function Rasterizer() {
           <p>Since the points are in a space relative to the camera, all we need to do is check if <InlineMath math='N_z'/> is negative. If yes, we render the face, otherwise we skip the calculations.</p>
           <p>On average, this eliminates roughly half (~50%) of unnecessary renders, saving time and computational power.</p>
 
-          <h2 className='subsubheader-project'>Shading</h2>
+          <CopyableHeader id="shading" className="subsubheader-project">Shading</CopyableHeader>
           <div className="grid md:grid-cols-2 gap-8">
             <img className="col-span-1" src={Shading}/>
-            <p>Finally, I had to implement some form of face differentiation, to prevent the rendered object from looking flat. Originally, I just assigned each face a random color, before switching to a better shading calculation. This involved calculating the ratio of the normal vector of each face to the forward vector of the camera and multiplying the face color by that. This changes its darkness based on the extent to which it's facing the camera.</p>
+            <div>
+              <p>Finally, I had to implement some form of face differentiation, to prevent the rendered object from looking flat. Originally, I just assigned each face a random color, before switching to a flat shading calculation. It is done by obtaining the dot product of the light vector and each face's normal vector, and using it to scale the color intensity. This changes its darkness based on the extent to which it's facing a given light source direction.</p>
+            </div>
           </div>
+          <p className='md:mt-2'>If the dot product between the normal vector and the light vector is negative, the two are at an angle greater than 90 degrees, thus facing away for each other. Faces with a negative dot product have their color intensity further scaled by 0.1, as they are facing away from the light source and minimal light should hit them.</p>
 
           <h1 className='subheader-project'>Fun Fact:</h1>
           <div className="grid md:grid-cols-2 gap-8">
